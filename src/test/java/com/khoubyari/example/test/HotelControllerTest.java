584eed223541197d2e0575f8af3d9f672bf45922
Deblock Adrien <adrien.deblock@davidson.fr>
2018-04-17 09:39:37 +0200

    add : Info in Test

--- src/test/java/com/khoubyari/example/test/HotelControllerTest.java
+++ src/test/java/com/khoubyari/example/test/HotelControllerTest.java
@@ -83,6 +83,7 @@ public class HotelControllerTest {
 
     @Test
     public void should_create_hotel() throws Exception {
+        System.out.println("Begin Should create hotel");
         Hotel hotelCreate = new Hotel();
         hotelCreate.setName("Create");
         hotelCreate.setRating(4);
@@ -103,20 +104,24 @@ public class HotelControllerTest {
         hotelCreate.setId(returnedHotel.getId());
 
         assertEquals(hotelCreate, returnedHotel);
+        System.out.println("End Should create hotel");
     }
 
     @Test
     public void should_find_existing_hotel() throws Exception {
+        System.out.println("Begin Should find existing hotel");
         final MockHttpServletRequestBuilder req = get(BASE_ROUTE + this.hotel.getId());
         final MvcResult result = this.mockMvc.perform(req).andExpect(status().isOk()).andReturn();
 
         final byte[] jsonArray = result.getResponse().getContentAsByteArray();
         final Hotel returnedHotel = OBJECT_MAPPER.readerFor(Hotel.class).readValue(jsonArray);
         assertEquals(this.hotel, returnedHotel);
+        System.out.println("End Should find existing hotel");
     }
 
     @Test
     public void should_update_existing_hotel() throws Exception {
+        System.out.println("Begin Should update existing hotel");
         final Hotel updateHotel = new Hotel();
         updateHotel.setDescription("MAJ Desc");
         updateHotel.setCity(hotel.getCity());
@@ -135,10 +140,12 @@ public class HotelControllerTest {
         final Hotel returnedHotel = OBJECT_MAPPER.readerFor(Hotel.class).readValue(jsonArray);
 
         assertEquals(updateHotel, returnedHotel);
+        System.out.println("End Should update existing hotel");
     }
 
     @Test(expected = NestedServletException.class)
     public void should_fail_updating_if_hotel_not_exist() throws Exception {
+        System.out.println("Begin Should fail_updating_if_hotel_not_exist");
         final Hotel updateHotel = new Hotel();
         updateHotel.setDescription("MAJ Desc");
         updateHotel.setCity(hotel.getCity());
@@ -152,25 +159,31 @@ public class HotelControllerTest {
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound())
                 .andReturn();
+        System.out.println("End Should fail_updating_if_hotel_not_exist");
     }
 
     @Test
     public void should_delete_existing_hotel() throws Exception {
+        System.out.println("Begin Should delete_existing_hotel");
         final MockHttpServletRequestBuilder req = delete(BASE_ROUTE + this.hotel.getId());
         this.mockMvc.perform(req).andExpect(status().isOk());
+        System.out.println("End Should delete_existing_hotel");
     }
 
     @Test
     public void should_return_all_paginated_hotel() throws Exception {
+        System.out.println("Begin Should return_all_paginated_hotel");
         final MvcResult result = this.mockMvc.perform(get(BASE_ROUTE)).andExpect(status().isOk()).andReturn();
 
         final byte[] jsonArray = result.getResponse().getContentAsByteArray();
         final Page<Hotel> returnedHotels = OBJECT_MAPPER.readerFor(new TypeReference<PageHelper<Hotel>>(){}).readValue(jsonArray);
         assertEquals(2, returnedHotels.getTotalElements());
+        System.out.println("End Should return_all_paginated_hotel");
     }
 
     @Test
     public void should_return_hotel_find_by_city() throws Exception {
+        System.out.println("Begin Should return_hotel_find_by_city");
         final MvcResult result = this.mockMvc.perform(get(BASE_ROUTE+"/byCity/"+ hotel1.getCity())).andExpect(status().isOk()).andReturn();
 
         final byte[] jsonArray = result.getResponse().getContentAsByteArray();
@@ -180,15 +193,20 @@ public class HotelControllerTest {
         Hotel returnedHotel = new Hotel();
         if(returnedHotels.iterator().hasNext()) returnedHotel = returnedHotels.iterator().next();
         assertEquals(hotel1, returnedHotel);
+        System.out.println("End Should return_hotel_find_by_city");
     }
 
     @Test
     public void test_suite_fibonnacci_courte(){
+        System.out.println("Begin suite_fibonnacci_courte");
         assertEquals(89, Application.fibonacci(11, 0, 1));
+        System.out.println("End suite_fibonnacci_courte");
     }
 
     @Test
     public void test_suite_fibonnacci_use_puissance(){
+        System.out.println("Begin suite_fibonnacci_use_puissance");
         assertEquals(433494437, Application.fibonacci(43, 0, 1));
+        System.out.println("End suite_fibonnacci_use_puissance");
     }
 }
